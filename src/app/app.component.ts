bde7231e6e938a0743b9a48dfd955927cb3c62ff
tasneem343 <tasneemts343@gmail.com>
2025-04-23 15:49:52 +0200

    edit scroll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,23 @@
 import { Component } from '@angular/core';
-import { NavigationEnd, Router } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  constructor(private router: Router) {
+  constructor(private router: Router, private spinner: NgxSpinnerService) {
     this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
+        this.spinner.show();
         setTimeout(() => {
           window.scrollTo({ top: 0, behavior: 'smooth' });
-        }, 0);
+          this.spinner.hide();
+        }, 100);
       }
     });
   }

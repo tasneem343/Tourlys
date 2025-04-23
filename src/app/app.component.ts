9409d11e62a0e8410f91da089dbba3fb16fe2cb6
tasneem343 <tasneemts343@gmail.com>
2025-04-23 15:44:09 +0200

    Edit Image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,8 @@
 import { Component } from '@angular/core';
-import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './Components/header/header.component';
-import { HeroComponent } from './Components/hero/hero.component';
-import { PopularDestinationsComponent } from './Components/popular-destinations/popular-destinations.component';
-import { PackagesComponent } from './Components/packages/packages.component';
-import { GalleryComponent } from './Components/gallery/gallery.component';
-
-import { ContactComponent } from './Components/contact/contact.component';
-import { FooterComponent } from './Components/footer/footer.component';
-import { ToursearchComponent } from './Components/toursearch/toursearch.component';
-import { AdminComponent } from './Layouts/admin/admin.component';
-import { NgxSpinnerComponent } from 'ngx-spinner';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NgxSpinnerComponent,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
@@ -22,7 +10,9 @@ export class AppComponent {
   constructor(private router: Router) {
     this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
-        window.scrollTo(0, 0);
+        setTimeout(() => {
+          window.scrollTo({ top: 0, behavior: 'smooth' });
+        }, 0);
       }
     });
   }
